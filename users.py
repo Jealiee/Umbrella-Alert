ebb782a46a7bef140bb623c416d0777d2bdba55f
Jealie <adamus_julia@wp.pl>
2026-03-08 15:39:35 +0100

    Store timezone info

--- users.py
+++ users.py
@@ -12,19 +12,23 @@ def save_users(users):
     with open(CONFIG_FILE, 'w') as file:
         yaml.safe_dump({"users":users}, file, sort_keys =False)
 
-def add_user(city, latitude, longitude, chat_id):
+def add_user(city, latitude, longitude, chat_id, timezone):
     users = load_users()
 
     if any(u['chat_id'] == chat_id for u in users):
         print('User already exists')
         return  
-    new_user ={'chat_id': chat_id, 'city': city, 'latitude': latitude, 'longitude':longitude}
+    new_user ={'chat_id': chat_id, 
+               'city': city, 
+               'latitude': latitude, 
+               'longitude':longitude, 
+               'timezone': timezone}
 
     users.append(new_user)
     save_users(users)
     print ('User added successfully.')
 
-def update_user(chat_id, city, latitude, longitude):
+def update_user(chat_id, city, latitude, longitude, timezone):
 
     with open(CONFIG_FILE, "r") as file:
         data = yaml.safe_load(file)
@@ -33,7 +37,11 @@ def update_user(chat_id, city, latitude, longitude):
 
     for user in users:
         if user["chat_id"] == chat_id:
-            user.update({"city": city, "latitude": latitude, "longitude": longitude})
+            user.update({
+                "city": city, 
+                "latitude": latitude, 
+                "longitude": longitude,
+                'timezone': timezone})
             save_users(users)
             print("User updated successfully.")
             return
