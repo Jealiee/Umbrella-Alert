6671511f9b62d9904892138d62ce5dc0a1c3de8c
Jealie <adamus_julia@wp.pl>
2026-03-08 15:33:45 +0100

    Get user timezone

--- geocode.py
+++ geocode.py
@@ -1,4 +1,5 @@
 import requests
+from timezonefinder import TimezoneFinder
 
 def get_coordinates(city_name):
     url="https://nominatim.openstreetmap.org/search"
@@ -13,3 +14,7 @@ def get_coordinates(city_name):
     latitude = float(data[0]["lat"])
     longitude = float(data[0]["lon"])
     return latitude, longitude
+
+def get_timezone(latitude, longitude):
+    tf = TimezoneFinder()
+    return tf.timezone_at(latitude = latitude, longitude = longitude)
